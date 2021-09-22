c3ab775a47b769286cec5b972485f0c6e3735c46
FKMLJF <vincze.jozsef.develop@gmail.com>
2021-09-22 15:21:14 +0200

    Fixing the error: “A possible object cycle was detected”

--- PackageManagerAPI/PackageManagerAPI/Startup.cs
+++ PackageManagerAPI/PackageManagerAPI/Startup.cs
@@ -54,6 +54,9 @@ namespace PackageManagerAPI
                 };
             });
             services.AddSession();
+            services.AddControllers().AddNewtonsoftJson(options =>
+                options.SerializerSettings.ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore
+            );
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
